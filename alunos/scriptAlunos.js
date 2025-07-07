3ef0218f08be2db9b622de6b7bdddb225123ed90
Kauã Betineli <kauaikoto@gmail.com>
2025-07-07 16:40:11 -0300

    feat: alunos

---
+++ alunos/scriptAlunos.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let res = document.getElementById("listagem")
+    res.innerHTML = "Buscando dados..."
+    fetch('https://api.npoint.io/e406bcc22a2e7636e2e8')
+    .then(response => response.json())
+    .then(data => {
+        let tabela = `
+            <table border="0" cellpadding="6">
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Nome</th>
+                        <th>Sobrenome</th>
+                        <th>Matrícula</th>
+                        <th>Telefone</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+        `;
+
+        data.forEach(aluno => {
+            tabela += `
+                <tr>
+                    <td>${aluno.codAluno}</td>
+                    <td>${aluno.nome}</td>
+                    <td>${aluno.sobrenome}</td>
+                    <td>${aluno.matricula}</td>
+                    <td>${aluno.telefone}</td>
+                    <td>${aluno.email}</td>
+                </tr>
+            `;
+        });
+
+        tabela += `</tbody></table>`;
+        res.innerHTML = tabela;
+    })
+
+
+    // alert("DOM carregado com DOMContentLoaded!");
+  });
